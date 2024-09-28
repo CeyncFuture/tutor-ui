5944b5aae6d11a549784f9edd8760a71e899aaff
CharithNiroshan <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>
2024-09-28 23:01:45 +0530

    Added changes related to admin home

---
+++ src/reducers/admin.js
@@ -0,0 +1,32 @@
+import {createReducer} from '../utils/utils';
+import {GET_QUESTIONS, GET_TUTORS} from "../actions/constants/admin";
+import {SHOW_LOADER} from "../actions/constants/user";
+
+const admin = {
+    tutors: [],
+    questions: [],
+    isLoading: false
+};
+
+const states = {
+    [GET_QUESTIONS]: (state, payload) => {
+        return {
+            ...state,
+            questions: payload
+        };
+    },
+    [GET_TUTORS]: (state, payload) => {
+        return {
+            ...state,
+            questions: payload,
+        };
+    },
+    [SHOW_LOADER]: (state, payload) => {
+        return {
+            ...state,
+            isLoading: payload
+        }
+    },
+};
+
+export default createReducer(admin, states);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import user from './user';
 import common from "./common";
+import admin from "./admin";
 
 export default combineReducers({
   user,
-  common
+  common,
+  admin
 });
